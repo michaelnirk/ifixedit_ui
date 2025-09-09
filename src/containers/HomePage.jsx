243020248c6a5067bef96cdf837c5f1602eff418
michaelnirk <michael.nirk@gmail.com>
2025-09-09 19:10:23 -0400

    Initial commit

---
+++ src/containers/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div style={{ height: "100vh", width: "100vw", backgroundColor: "lightblue" }}>HomePage</div>;
+}
+
+export default HomePage;
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "@/containers/HomePage.jsx";
+import ContactPage from "@/containers/Contact.js";
+import NotFoundPage from "@/containers/NotFoundPage.jsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />
+  },
+  {
+    path: "*", 
+    element: <NotFoundPage />
+  },
+]);
