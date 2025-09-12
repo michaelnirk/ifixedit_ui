73df3f3ae857fc307e63d30450f6e1ce05e174d5
michaelnirk <michael.nirk@gmail.com>
2025-09-11 20:46:25 -0400

    add redux

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Counter from "@/components/Counter";
 
 const HomePage = () => {
-  return <div style={{ height: "100vh", width: "100vw", backgroundColor: "lightblue" }}>HomePage</div>;
+  return (
+    <div style={{ height: "100vh", width: "100vw", backgroundColor: "lightblue", padding: "20px" }}>
+      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>Home Page</h1>
+      <Counter />
+    </div>
+  );
 }
 
 export default HomePage;
