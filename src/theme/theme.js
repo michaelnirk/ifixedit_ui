35aabdcc4c658650f21b701765c2daa067935601
michaelnirk <michael.nirk@gmail.com>
2026-02-07 15:39:09 -0500

    Hide spinner buttons

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -11,6 +11,20 @@ const customTheme = createTheme({
 					}
 				}
 			}
+		},
+		MuiTextField: {
+			styleOverrides: {
+				root: {
+					// Target the input wrapper
+					'& input[type=number]': {
+						MozAppearance: 'textfield' // Firefox
+					},
+					'& input[type=number]::-webkit-outer-spin-button, & input[type=number]::-webkit-inner-spin-button': {
+						// Hide default in Chrome/Safari
+						display: 'none'
+					}
+				}
+			}
 		}
 	}
 });
